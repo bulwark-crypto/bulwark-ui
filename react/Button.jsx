7c75fd98dace77a8046dca037530d257323a09f0
dustinengle <dustin.engle@gmail.com>
2018-08-13 10:01:58 -0700

    add props validation and defaults to button component

--- react/Button.jsx
+++ react/Button.jsx
@@ -1,8 +1,20 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 
 export default class Button extends React.Component {
+  static defaultProps = {
+    as: 'button',
+    mods: {}
+  }
+
+  static propTypes = {
+    as: PropTypes.any,
+    children: PropTypes.any.isRequired,
+    mods: PropTypes.object.isRequired
+  }
+
   render () {
-    const {children, ...mods} = this.props
-    return <button block='b' mods={mods}>{children}</button>
+    const {as, children, ...mods} = this.props
+    return <as block='b' mods={mods}>{children}</as>
   }
 }
