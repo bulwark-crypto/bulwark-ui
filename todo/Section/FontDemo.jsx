36b3b9053b81283b0999a76bb572e70af50ce3fb
dustinengle <dustin.engle@gmail.com>
2018-08-13 11:37:18 -0700

    added font demo

---
+++ todo/Section/FontDemo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Row from '../../react/Row'
+
+const FontDemo = () => (
+  <div>
+    <div className="hero">Hero</div>
+    <h1>H1</h1>
+    <h2>H2</h2>
+    <h3>H3</h3>
+    <h4>H4</h4>
+    <h5>H5</h5>
+    <div>Body</div>
+    <p>Paragraph</p>
+  </div>
+)
+
+export default FontDemo
