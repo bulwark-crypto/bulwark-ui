ec27f93f5a1cde3aa7e9b4257c5baa577e57e5ac
Penple03 <penple@penple.org>
2018-08-20 21:08:31 -0700

    Cleaning up the buttons

--- react/Button.jsx
+++ react/Button.jsx
@@ -3,23 +3,20 @@ import {pickRest} from '../lib/utils'
 import PropTypes from 'prop-types'
 
 const Button = props => {
-  const [ mods, {children, as, block, ...rest} ] = pickRest(props, ['primary', 'secondary', 'icon', 'disabled', 'outline', 'text'])
+  const [ mods, {children, as, block, ...rest} ] = pickRest(props, ['primary', 'secondary', 'disabled', 'outline', 'text'])
   const As = as
-  return <As block={block} mods={mods} {...rest}>{children}</As>
+  return <As block='button' mods={mods} {...rest}>{children}</As>
 }
 
 Button.defaultProps = {
-  as: 'button',
-  block: 'button'
+  as: 'button'
 }
 
 Button.propTypes = {
   as: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
-  block: PropTypes.string,
   primary: PropTypes.bool,
   secondary: PropTypes.bool,
   disabled: PropTypes.bool,
-  icon: PropTypes.bool,
   outline: PropTypes.bool,
   text: PropTypes.bool
 }
