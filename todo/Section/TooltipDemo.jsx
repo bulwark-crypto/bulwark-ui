dc539effb77075224093668d3488d6efaa212bed
dustinengle <dustin.engle@gmail.com>
2018-08-21 14:10:56 -0700

    #23 tooltip added

--- todo/Section/TooltipDemo.jsx
+++ todo/Section/TooltipDemo.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
+import Button from '../../react/Button'
+import Icon from '../../react/Icon'
 import Tooltip from '../../react/Tooltip'
 
 const TooltipDemo = () => (
   <div>
     <h3>Tooltips</h3>
-    <Tooltip content="I am on the top">Default Top</Tooltip>
-    <Tooltip content="I am on the bottom">Default Top</Tooltip>
-    <Tooltip content="I am on the right">Default Top</Tooltip>
-    <Tooltip content="I am on the left">Default Top</Tooltip>
+    <Tooltip top content="I am on the top"><Button>Default Top</Button></Tooltip>
+    <Tooltip primary bottom content="I am on the bottom"><Button primary>Primary Bottom</Button></Tooltip>
+    <Tooltip urgent right content="I am on the right"><Button secondary outline>Urgent Right</Button></Tooltip>
+    <Tooltip left content="I am on the left"><Button icon><Icon k='home' /></Button></Tooltip>
   </div>
 )
 
