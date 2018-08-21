a7c0c3357988f70bd9422a5f599b5f9760919621
dustinengle <dustin.engle@gmail.com>
2018-08-21 14:20:08 -0700

    added more test for tooltip #23

--- test/Tooltip.test.js
+++ test/Tooltip.test.js
@@ -7,12 +7,14 @@ import Tooltip from '../react/Tooltip'
 describe('<Tooltip />', () => {
   it('renders as a tooltip', () => {
     const wrapper = shallow(<Tooltip content="testtip">testtip</Tooltip>)
+    expect(wrapper.is('div')).to.be.true()
     expect(wrapper.hasClass('tooltip')).to.be.true()
   })
 
   it('renders as a tooltip__content', () => {
     const wrapper = shallow(<Tooltip content="testtip">testtip</Tooltip>)
     expect(wrapper.children()).to.have.length(2)
+    expect(wrapper.children().at(0).is('span')).to.be.true()
     expect(wrapper.children().at(0).html()).to.equal('<span class="tooltip__content">testtip</span>')
     expect(wrapper.children().at(1).text()).to.equal('testtip')
   })
