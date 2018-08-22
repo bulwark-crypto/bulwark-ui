be68dc55ffa7311890cdb770bb8e61b412c645fb
dustinengle <dustin.engle@gmail.com>
2018-08-21 19:45:34 -0700

    updated to tooltip demo

--- todo/Section/TooltipDemo.jsx
+++ todo/Section/TooltipDemo.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Button from '../../react/Button'
-import Icon from '../../react/Icon'
+import IconButton from '../../react/IconButton'
 import Tooltip from '../../react/Tooltip'
 
 const TooltipDemo = () => (
@@ -10,7 +10,7 @@ const TooltipDemo = () => (
     <Tooltip top content="I am on the top"><Button>Default Top</Button></Tooltip>
     <Tooltip primary bottom content="I am on the bottom"><Button primary>Primary Bottom</Button></Tooltip>
     <Tooltip urgent right content="I am on the right"><Button secondary outline>Urgent Right</Button></Tooltip>
-    <Tooltip left content="I am on the left"><Button icon><Icon k='home' /></Button></Tooltip>
+    <Tooltip left content="I am on the left"><IconButton icon k='home' /></Tooltip>
   </div>
 )
 
