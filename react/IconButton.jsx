47e53a24116d562ef1932b515e15162a793cecc4
dustinengle <dustin.engle@gmail.com>
2018-09-04 13:46:28 -0700

    fix error for icon button

--- react/IconButton.jsx
+++ react/IconButton.jsx
@@ -5,25 +5,26 @@ import {pickRest} from '../lib/utils'
 import Icon from './Icon'
 
 export default class IconButton extends React.Component {
+  static defaultProps = {
+    as: 'button'
+  }
+
+  static propTypes = {
+    as: PropTypes.string.isRequired,
+    icon: PropTypes.bool,
+    k: PropTypes.string.isRequired,
+    primary: PropTypes.bool,
+    secondary: PropTypes.bool,
+    disabled: PropTypes.bool
+  }
+
   render () {
-    const [ mods, {children, as, k, ...rest} ] = pickRest(this.props, ['primary', 'secondary', 'disabled'])
+    const [ mods, {children, as, icon, k, ...rest} ] = pickRest(this.props, ['primary', 'secondary', 'disabled'])
     const As = as
     return (
-      <As block='button' mods={{...mods, icon: true}} {...rest}>
+      <As block='button' mods={{...mods, icon: icon || true}} {...rest}>
         <Icon {...{k}} />{children}
       </As>
     )
   }
-}
-
-IconButton.defaultProps = {
-  as: 'button'
-}
-
-IconButton.propTypes = {
-  as: PropTypes.string.isRequired,
-  k: PropTypes.string.isRequired,
-  primary: PropTypes.bool,
-  secondary: PropTypes.bool,
-  disabled: PropTypes.bool
 }
