65ddfb74d7f09c0eb40b5331cc67716e9b5151cd
dustinengle <dustin.engle@gmail.com>
2018-09-12 12:45:54 -0700

    animation fixes

--- demo/web/Section/MenuDemo.jsx
+++ demo/web/Section/MenuDemo.jsx
@@ -12,21 +12,17 @@ const MenuDemo = () => (
   <div>
     <h3>Menu</h3>
     <Row>
-      <Col xs={12} sm={12} md={6} lg={3}>
+      <Col xs={12}>
         <Menu component={<Button>Menu One</Button>}>
           {[10, 20, 40, 50].map(x => (
             <MenuItem key={x} onClick={fn}>{x}</MenuItem>
           ))}
         </Menu>
-      </Col>
-      <Col xs={12} sm={12} md={6} lg={3}>
         <Menu component={<Button>Menu Two</Button>}>
           {['United States', 'Ireland', 'United Kingdom', 'Brazil', 'China', 'Peru'].map(x => (
             <MenuItem key={x} onClick={fn}>{x}</MenuItem>
           ))}
         </Menu>
-      </Col>
-      <Col xs={12} sm={12} md={6} lg={3}>
         <Menu component={<Button>Menu Three</Button>}>
           <MenuItem onClick={fn}><Icon k='envelope' />Send</MenuItem>
           <MenuItem onClick={fn}><Icon k='money-bill' />Transactions</MenuItem>
