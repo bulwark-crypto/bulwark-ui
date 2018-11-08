083646f3810a90b09a4cbb4c0e535edc874d1acf
Penple03 <penple@penple.org>
2018-11-07 21:24:20 -0800

    Begin work on moving to a CSS-in-JS model.

--- demo/web/App.jsx
+++ demo/web/App.jsx
@@ -4,6 +4,10 @@ import {HashRouter, Link, Route, Switch} from 'react-router-dom'
 import React from 'react'
 import { render } from 'react-dom'
 
+import {ThemeProvider} from 'styled-components'
+
+import {light} from '../../lib/Themes'
+
 import Github from './Github'
 import Home from './Home'
 import SASS from './SASS'
@@ -41,66 +45,68 @@ class App extends React.Component {
 
   render () {
     return (
-      <HashRouter>
-        <div>
-          <Navbar onDrawer={this.handleDrawer}>
-            <NavbarLink k='home' to='#/'>Home</NavbarLink>
-          </Navbar>
-          <Drawer onDrawer={this.handleDrawer} open={this.state.menu}>
-            <DrawerHeader title='Bulwark' subtitle='React UI Framework' />
-            <DrawerLink k='palette'><Link to='/sass'>SASS</Link></DrawerLink>
-            <DrawerLink k='code'><Link to='/github'>Source Code</Link></DrawerLink>
-            <DrawerHeader subtitle='Components' />
-            <DrawerLink><Link to='/badge'>Badge</Link></DrawerLink>
-            <DrawerLink><Link to='/button'>Button</Link></DrawerLink>
-            <DrawerLink><Link to='/card'>Card</Link></DrawerLink>
-            <DrawerLink><Link to='/chart'>Chart</Link></DrawerLink>
-            <DrawerLink><Link to='/checkbox'>Checkbox/Switch</Link></DrawerLink>
-            <DrawerLink><Link to='/grid'>Grid</Link></DrawerLink>
-            <DrawerLink><Link to='/menu'>Menu</Link></DrawerLink>
-            <DrawerLink><Link to='/modal'>Modal</Link></DrawerLink>
-            <DrawerLink><Link to='/pagination'>Pagination</Link></DrawerLink>
-            <DrawerLink><Link to='/panel'>Panel</Link></DrawerLink>
-            <DrawerLink><Link to='/progress'>Progress</Link></DrawerLink>
-            <DrawerLink><Link to='/radio'>Radio</Link></DrawerLink>
-            <DrawerLink><Link to='/selectinput'>Select Input</Link></DrawerLink>
-            <DrawerLink><Link to='/stepper'>Stepper</Link></DrawerLink>
-            <DrawerLink><Link to='/tab'>Tab</Link></DrawerLink>
-            <DrawerLink><Link to='/table'>Table</Link></DrawerLink>
-            <DrawerLink><Link to='/textinput'>Text Input</Link></DrawerLink>
-            <DrawerLink><Link to='/tooltip'>Tooltip</Link></DrawerLink>
-            <DrawerLink><Link to='/font'>Typography</Link></DrawerLink>
-          </Drawer>
-          <div block='wrapper'>
-            <Switch>
-              <Route path='/font' component={FontDemo} />
-              <Route path='/grid' component={GridDemo} />
-              <Route path='/button' component={ButtonDemo} />
-              <Route path='/card' component={CardDemo} />
-              <Route path='/tooltip' component={TooltipDemo} />
-              <Route path='/badge' component={BadgeDemo} />
-              <Route path='/modal' component={ModalDemo} />
-              <Route path='/checkbox' component={CheckboxDemo} />
-              <Route path='/radio' component={RadioDemo} />
-              <Route path='/panel' component={PanelDemo} />
-              <Route path='/textinput' component={TextInputDemo} />
-              <Route path='/selectinput' component={SelectInputDemo} />
-              <Route path='/menu' component={MenuDemo} />
-              <Route path='/tab' component={TabDemo} />
-              <Route path='/pagination' component={PaginationDemo} />
-              <Route path='/stepper' component={StepperDemo} />
-              <Route path='/table' component={TableDemo} />
-              <Route path='/chart' component={ChartDemo} />
-              <Route path='/progress' component={ProgressBarDemo} />
+      <ThemeProvider theme={light}>
+        <HashRouter>
+          <div>
+            <Navbar onDrawer={this.handleDrawer}>
+              <NavbarLink k='home' to='#/'>Home</NavbarLink>
+            </Navbar>
+            <Drawer onDrawer={this.handleDrawer} open={this.state.menu}>
+              <DrawerHeader title='Bulwark' subtitle='React UI Framework' />
+              <DrawerLink k='palette'><Link to='/sass'>SASS</Link></DrawerLink>
+              <DrawerLink k='code'><Link to='/github'>Source Code</Link></DrawerLink>
+              <DrawerHeader subtitle='Components' />
+              <DrawerLink><Link to='/badge'>Badge</Link></DrawerLink>
+              <DrawerLink><Link to='/button'>Button</Link></DrawerLink>
+              <DrawerLink><Link to='/card'>Card</Link></DrawerLink>
+              <DrawerLink><Link to='/chart'>Chart</Link></DrawerLink>
+              <DrawerLink><Link to='/checkbox'>Checkbox/Switch</Link></DrawerLink>
+              <DrawerLink><Link to='/grid'>Grid</Link></DrawerLink>
+              <DrawerLink><Link to='/menu'>Menu</Link></DrawerLink>
+              <DrawerLink><Link to='/modal'>Modal</Link></DrawerLink>
+              <DrawerLink><Link to='/pagination'>Pagination</Link></DrawerLink>
+              <DrawerLink><Link to='/panel'>Panel</Link></DrawerLink>
+              <DrawerLink><Link to='/progress'>Progress</Link></DrawerLink>
+              <DrawerLink><Link to='/radio'>Radio</Link></DrawerLink>
+              <DrawerLink><Link to='/selectinput'>Select Input</Link></DrawerLink>
+              <DrawerLink><Link to='/stepper'>Stepper</Link></DrawerLink>
+              <DrawerLink><Link to='/tab'>Tab</Link></DrawerLink>
+              <DrawerLink><Link to='/table'>Table</Link></DrawerLink>
+              <DrawerLink><Link to='/textinput'>Text Input</Link></DrawerLink>
+              <DrawerLink><Link to='/tooltip'>Tooltip</Link></DrawerLink>
+              <DrawerLink><Link to='/font'>Typography</Link></DrawerLink>
+            </Drawer>
+            <div block='wrapper'>
+              <Switch>
+                <Route path='/font' component={FontDemo} />
+                <Route path='/grid' component={GridDemo} />
+                <Route path='/button' component={ButtonDemo} />
+                <Route path='/card' component={CardDemo} />
+                <Route path='/tooltip' component={TooltipDemo} />
+                <Route path='/badge' component={BadgeDemo} />
+                <Route path='/modal' component={ModalDemo} />
+                <Route path='/checkbox' component={CheckboxDemo} />
+                <Route path='/radio' component={RadioDemo} />
+                <Route path='/panel' component={PanelDemo} />
+                <Route path='/textinput' component={TextInputDemo} />
+                <Route path='/selectinput' component={SelectInputDemo} />
+                <Route path='/menu' component={MenuDemo} />
+                <Route path='/tab' component={TabDemo} />
+                <Route path='/pagination' component={PaginationDemo} />
+                <Route path='/stepper' component={StepperDemo} />
+                <Route path='/table' component={TableDemo} />
+                <Route path='/chart' component={ChartDemo} />
+                <Route path='/progress' component={ProgressBarDemo} />
 
-              <Route path='/github' component={Github} />
-              <Route path='/sass' component={SASS} />
-              <Route path='/' component={Home} />
-            </Switch>
+                <Route path='/github' component={Github} />
+                <Route path='/sass' component={SASS} />
+                <Route path='/' component={Home} />
+              </Switch>
+            </div>
+            <div style={{height: 100}} />
           </div>
-          <div style={{height: 100}} />
-        </div>
-      </HashRouter>
+        </HashRouter>
+      </ThemeProvider>
     )
   }
 }
--- web/Badge.jsx
+++ web/Badge.jsx
@@ -1,14 +1,48 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import {pickRest} from '../lib/utils'
+import styled, {css} from 'styled-components'
+import {pulse, swiftEaseIn} from '../lib/Animations'
+
+const BadgeWrapper = styled.span`
+  position: relative;
+  z-index: 1;
+  ${props => props.primary ? css`
+    ${BadgeText} {
+      animation: ${pulse('primary')} 1.25s infinite cubic-bezier(0.66, 0, 0, 1);
+      background-color: ${props => props.theme.primary};
+    }
+  ` : ''}
+`
+const BadgeText = styled.span`
+  animation: ${pulse('default')} 1.25s infinite cubic-bezier(0.66, 0, 0, 1);
+  background-color: ${props => props.theme.black};
+  border-radius: 50%;
+  color: ${props => props.theme.white};
+  font-size: 14px;
+  height: 24px;
+  position: absolute;
+  right: -8px;
+  text-align: center;
+  top: -100%;
+  transition: ${swiftEaseIn};
+  width: 24px;
+  z-index: 2;
+
+  span {
+    left: 50%;
+    position: absolute;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+`
 
 const Badge = (props) => {
-  const [mods, {children, text, ...rest}] = pickRest(props, ['primary'])
+  const {children, text, ...rest} = props
   return (
-    <span block='badge' mods={mods} {...rest}>
-      <span block='badge' elem='text'><span>{text}</span></span>
+    <BadgeWrapper {...rest}>
+      <BadgeText><span>{text}</span></BadgeText>
       {children}
-    </span>
+    </BadgeWrapper>
   )
 }
 
