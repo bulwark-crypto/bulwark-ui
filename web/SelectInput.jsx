becf9cce9835884b7ac08bd37b72b2fa85222d94
Penple03 <penple@penple.org>
2018-11-27 18:11:19 -0800

    Port Inputs

--- web/SelectInput.jsx
+++ web/SelectInput.jsx
@@ -1,8 +1,136 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import {kill, pickRest} from '../lib/utils'
+import {kill} from '../lib/utils'
+import styled, {css} from 'styled-components'
 
 import Icon from './Icon'
+import { swiftEaseIn, swiftEaseInOut, animationSelectItem } from '../lib/Animations'
+
+const SelectInputWrapper = styled.div`
+  font-family: ${props => props.theme.fontPrimary};
+  font-size: 14px;
+  height: 55px;
+  padding-top: 18px;
+  position: relative;
+
+  ${props => props.active ? css`
+    ${SelectInputLabel} {
+      color: ${props => props.theme.secondary};
+      font-family: ${props => props.theme.fontPrimary};
+      font-size: 12px;
+      height: 11px;
+      line-height: 11px;
+      top: 0;
+    }
+
+    ${SelectInputArrow} { color: ${props => props.theme.black}; }
+  ` : ''}
+
+  ${props => props.focus && !props.native ? css`
+    ${SelectInputComponent} {
+      display: none;
+    }
+
+    ${SelectInputOptions} {
+      max-height: 230px;
+      transition: ${swiftEaseInOut};
+
+      ${SelectInputItem} {
+        opacity: 1;
+        transition: ${animationSelectItem};
+      }
+    }
+  ` : ''}
+`
+
+const SelectInputLabel = styled.label`
+  color: ${props => props.theme.secondary};
+  cursor: text;
+  font-family: ${props => props.theme.fontPrimary};
+  font-size: 14px;
+  height: 16px;
+  line-height: 16px;
+  min-width: 108px;
+  position: absolute;
+  top: 18px;
+  transition: ${swiftEaseIn};
+`
+
+const SelectInputArrow = styled.label`
+  color: ${props => props.theme.secondary};
+  float: right;
+  height: 5px;
+  position: absolute;
+  right: 0;
+  top: 19px;
+  width: 10px;
+`
+
+const SelectInputComponent = styled.select`
+  background: transparent;
+  border: 0;
+  color: ${props => props.theme.black};
+  line-height: 16px;
+  outline: none;
+  padding: 4px 0;
+  appearance: none;
+  border: 0 !important;
+  color: ${props => props.theme.black};
+  text-indent: 0.1px;
+  text-overflow: '';
+
+  ::-ms-expand {
+    display: none;
+  }
+
+  option {
+    border: 0;
+    color: ${props => props.theme.black};
+    max-height: 0;
+  }
+
+  border-color: transparent;
+  border-bottom: 1px solid #f2f2f2;
+  font-family: ${props => props.theme.fontPrimary};
+  font-size: 14px;
+  min-width: 108px;
+  width: 100%;
+`
+
+const SelectInputOptions = styled.div`
+  background-color: ${props => props.theme.white};
+  border-radius: 5px;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.25);
+  display: block;
+  left: 50%;
+  max-height: 0;
+  min-width: 108px;
+  overflow-x: hidden;
+  overflow-y: auto;
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  z-index: 4;
+`
+
+const SelectInputItem = styled.div`
+  cursor: pointer;
+  font-family: ${props => props.theme.fontPrimary};
+  font-size: 14px;
+  height: 27px;
+  line-height: 19px;
+  min-width: 108px;
+  opacity: 0;
+  padding: 6px 11px;
+  width: 100%;
+
+  :hover { background-color: rgba(124, 124, 124, 0.1); }
+
+  ${props => props.active ? css`
+    background-color: rgba(124, 124, 124, 0.2);
+  ` : ''}
+`
 
 export default class SelectInput extends React.Component {
   static defaultProps = {
@@ -67,10 +195,12 @@ export default class SelectInput extends React.Component {
   }
 
   render () {
-    const [mods, {defaultValue, label, message, name, notempty, options, value, ...rest}] = pickRest(this.props, ['error', 'native'])
+    const {defaultValue, label, message, name, notempty, options, value, ...rest} = this.props
 
-    if (this.state.isFocus) mods.focus = true
-    if (mods.focus || !!this.state.value) mods.active = true
+    let focus = false
+    if (this.state.isFocus) focus = true
+    let active = focus
+    if (this.state.value) active = true
 
     const inputProps = {
       autoComplete: 'off',
@@ -84,27 +214,27 @@ export default class SelectInput extends React.Component {
     const adjustedOptions = notempty ? options : [{text: '', value: ''}, ...options]
 
     return (
-      <div block='input' mods={mods}>
+      <SelectInputWrapper focus={focus} active={active} {...rest}>
         {!!label &&
-          <label block='input' elem='label' htmlFor={rest.name} onClick={this.handleLabelClick}>
+          <SelectInputLabel htmlFor={rest.name} onClick={this.handleLabelClick}>
             {label}
-          </label>
+          </SelectInputLabel>
         }
-        <div block='input' elem='arrow'><Icon k='caret-down' /></div>
-        <select block='input' elem='component' {...rest} {...inputProps}>
+        <SelectInputArrow><Icon k='caret-down' /></SelectInputArrow>
+        <SelectInputComponent {...rest} {...inputProps}>
           {adjustedOptions.map((o, i) => (<option key={i} value={o.value}>{o.text}</option>))}
-        </select>
-        {!mods.native &&
-          <div block='input' elem='options'>
+        </SelectInputComponent>
+        {!this.props.native &&
+          <SelectInputOptions>
             {adjustedOptions.map((o, i) => (
-              <div block='input__options' elem='item' key={i} mods={{active: o.value === this.state.value}} onClick={ev => this.handleSelect(ev, o)}>
+              <SelectInputItem key={i} active={o.value === this.state.value} onClick={ev => this.handleSelect(ev, o)}>
                 {o.text}
-              </div>
+              </SelectInputItem>
             ))}
-          </div>
+          </SelectInputOptions>
         }
         {!!message && <div block='input' elem='message'>{message}</div>}
-      </div>
+      </SelectInputWrapper>
     )
   }
 }
